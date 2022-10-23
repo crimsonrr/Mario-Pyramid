200568114bfc08fa83eb6e6cf6a48ff9aabc878b
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2022-10-23 21:31:57 +0000

    Sun, Oct 23, 2022, 9:31 PM +00:00

--- mario/mario-less/mario.c
+++ mario/mario-less/mario.c
@@ -12,9 +12,24 @@ do {
 }
    while(height < 0 || height > 23);
 
+for (int i = 0; i < height; i++)
+{
+    // Print spaces
+    for (spaces = (height - i); spaces >= 2; spaces--)
+    {
+        printf(" ");
 
-
-
-
+    }
+    // Print hashes
+    for (hash = 0; hash <= (i + 1); hash++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
+}
+
+
+
+
 
