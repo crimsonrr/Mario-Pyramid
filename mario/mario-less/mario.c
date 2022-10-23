df8da94b545c040975b249b467d0596737c6f3cc
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2022-10-23 21:25:55 +0000

    Sun, Oct 23, 2022, 9:25 PM +00:00

--- mario/mario-less/mario.c
+++ mario/mario-less/mario.c
@@ -1,21 +1,3 @@
 #include <stdio.h>
 #include <cs50.h>
 
-    int main(void)
-
-    {
-        int n;
-        do
-        {
-           n = get_int("Size: ");
-        }
-         int i;{
-            // Something happens
-         }
-            while (x > 1 && x < 8);
-
-      while (n < 1);
-      // For each row..
-       for (int x = 0; x < n; x++)
-
-
