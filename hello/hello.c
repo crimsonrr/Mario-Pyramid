0f52ff29dd00f99fe35dff73e236dfef98f4fb52
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2023-05-22 14:31:36 -0400

    idk

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,10 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
+// asks for name
     string name = get_string("What is you name?\n");
-
-    printf("hello, %s\n", name);
+// replies to given name with a greeting
+    printf("Hello, %s\n!", name);
 }
