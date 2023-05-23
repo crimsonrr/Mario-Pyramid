60ceace1e4039b601e9cd7c083fb228245cff744
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2023-05-23 17:59:27 +0000

    Tue, May 23, 2023, 1:59 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,10 +3,13 @@
 
 int main(void)
 {
+     // For each row
      for (int i = 0; i < n; i++)
   {
+    // For each column
       for (int j = 0; j < n; j++)
       {
+        // Print a brick
           printf("#");
       }
       printf("\n");
