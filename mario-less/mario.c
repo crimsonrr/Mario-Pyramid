d2c31e6f00ea1bd1edb75af9fcdb8e309f96ba45
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2023-05-23 17:56:11 +0000

    Tue, May 23, 2023, 1:56 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+     for (int i = 0; i < n; i++)
+  {
+      for (int j = 0; j < n; j++)
+      {
+          printf("#");
+      }
+      printf("\n");
+  }
 }
