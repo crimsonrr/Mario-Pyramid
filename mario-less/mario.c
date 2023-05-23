c02090a79d0f65f404b12ce90c9f0c2bd586813a
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2023-05-23 18:51:30 +0000

    Tue, May 23, 2023, 2:51 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,16 +14,16 @@ int main()
 
     while (n < 1);
     // For each row
-    for (int i = 0; i<n; i++)
-  {
-      //  For each column
-      for (int j = 0; j < i+1; j++)
-      {
-        // Print a hash symbol
-          printf("#");
-      }
-      // Next line
-      printf("\n");
-  }
+    for (int i = 0; i < n; i++)
+    {
+        //  For each column
+        for (int j = 0; j < i + 1; j++)
+        {
+// Print a hash symbol
+            printf("#");
+        }
+// Next line
+        printf("\n");
+    }
 
 }
