cb97e4ba7346d58e2e9a0d172f672d368a6a32c1
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2023-05-23 18:32:46 +0000

    Tue, May 23, 2023, 2:32 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,46 +1,3 @@
-#include <stdio.h>
-#include <cs50.h>
-
-void buildPyramid(int height);
-
-int main(void)
-{
-    // Initialize the variable height
-    int height;
-
-    // Run the loop to get a value of height between 1 and 8, inclusive, from the user
-    do
-    {
-        height = get_int("Height: ");
-    }
-    while (height < 1 || height > 8);
-
-    // Call the function and pass height to it as a parameter
-    buildPyramid(height);
-}
-
-// Declare the function buildPyramid
-void buildPyramid(int height)
-{
-    // Adds a new line
-    for (int i = 0; i < height; i++)
-    {
-        // Adds spaces
-        for (int k = height - i; k > 1; k--)
-        {
-            printf(" ");
-        }
-        // Adds hashes
-        for (int j = 0; j <= i; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
-    }
-}
-
-
-
 
 #include<cs50.h>
 #include<stdio.h>
@@ -54,15 +11,18 @@ int main()
         // Asks for height
         n = get_int("Height: ");
     }
-    
-    while (n < 1);
 
+    while (n < 1);
+    // For each row
     for (int i = 0; i<n; i++)
   {
+      //  For each column
       for (int j = 0; j < i+1; j++)
       {
+        // Print a hash symbol
           printf("#");
       }
+      // Next line
       printf("\n");
   }
 
