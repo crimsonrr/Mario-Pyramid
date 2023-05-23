876ad2297782d5facf885dabf26ddba9eec35c5f
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2023-05-23 18:30:46 +0000

    Tue, May 23, 2023, 2:30 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -38,3 +38,32 @@ void buildPyramid(int height)
         printf("\n");
     }
 }
+
+
+
+
+#include<cs50.h>
+#include<stdio.h>
+
+int main()
+{
+
+    int n;
+    do
+    {
+        // Asks for height
+        n = get_int("Height: ");
+    }
+    
+    while (n < 1);
+
+    for (int i = 0; i<n; i++)
+  {
+      for (int j = 0; j < i+1; j++)
+      {
+          printf("#");
+      }
+      printf("\n");
+  }
+
+}
