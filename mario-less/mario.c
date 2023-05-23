7b83477db6a0faab33ab7f5fbf75104674fb2f7c
crimsonrr <115940858+crimsonrr@users.noreply.github.com>
2023-05-23 18:22:52 +0000

    Tue, May 23, 2023, 2:22 PM -04:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void buildPyramid(int height);
-
 int main(void)
 {
     // Initialize the variable height
@@ -15,26 +13,22 @@ int main(void)
     }
     while (height < 1 || height > 8);
 
-    // Call the function and pass height to it as a parameter
-    buildPyramid(height);
 }
 
-// Declare the function buildPyramid
-void buildPyramid(int height)
 {
-    // Loop to add a new line
+    // Adds a new line
     for (int i = 0; i < height; i++)
     {
-        // Loop to add spaces
+        // Adds spaces
         for (int k = height - i; k > 1; k--)
         {
             printf(" ");
         }
-        // Loop to add hashes
+        // Adds hashes
         for (int j = 0; j <= i; j++)
         {
             printf("#");
         }
         printf("\n");
     }
-} 
+}
